ead1b819065defaae8033cf94dbd46d1bd8db912
filipmazev <56910771+filipmazev@users.noreply.github.com>
2025-08-26 19:13:58 +0200

    Initial project setup
    Set up the initial ASP .NET API and Angular projects with some base configurations to start work; configured a database project in Visual Studio & set up Angular to work with https and find the appropriate .pem and .key files locally

---
+++ spacexinterop.api/Core/ServiceBuilder.cs
@@ -0,0 +1,10 @@
+﻿namespace spacexinterop.api.Core;
+
+public static class ServiceBuilder
+{
+    public static IServiceCollection AddDependencies(this IServiceCollection services)
+    {
+
+        return services;
+    }
+}
---
+++ spacexinterop.client/src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    apiPath: 'https://localhost:8080/',
+    dbVersion: 1,
+};
